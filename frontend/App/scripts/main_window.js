6ec01533299fcc14dfc65623c30cd35f4ce3a40c
Jannis Blossey <jannis@blossey.eu>
2020-04-02 20:38:08 +0200

    bug: async execution leads to keysAndCaps being read before assignment.

--- frontend/App/scripts/main_window.js
+++ frontend/App/scripts/main_window.js
@@ -1,124 +1,139 @@
-const request = require('request');
-const jetpack = require('fs-jetpack');
-const remote = require('electron').remote;
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable prefer-template */
+const { remote, ipcRenderer } = require('electron');
+const needle = require('needle');
 const storage = require('electron-json-storage');
-const main_process = remote.require('./main.js');
-const ipc = require('electron').ipcRenderer;
+const $ = require('jquery');
+const Promise = require('promise');
+// eslint-disable-next-line import/no-unresolved
+const metadatahandlerStub = require('./scripts/metadatahandlerStub');
 
-//'27c44ce2-ddbb-47ed-e8c6-1809382b6000';//TODO delete
+const mainProcess = remote.require('./main.js');
 
-let char_count, char_lim;
+// '27c44ce2-ddbb-47ed-e8c6-1809382b6000';// TODO delete
 
-//TODO handle clicks and multi-select
-//TODO enable user to delete items
+let charCount;
+let charLim;
+
+// TODO handle clicks and multi-select
+// TODO enable user to delete items
+
+function fetchDeeplCharCount(key) {
+  if (key === null) {
+    // eslint-disable-next-line no-use-before-define
+    getDeeplKey();
+    return;
+  }
+  needle.get('https://api.deepl.com/v2/usage?auth_key=' + key, (error, response) => {
+    if (!error && response.statusCode === 200) {
+      charCount = response.body.character_count;
+      charLim = response.body.character_limit;
+    }
+    // eslint-disable-next-line no-undef
+    $('#deepl_character_count').text('Characters left on Deepl: ' + (charLim - charCount));
+  });
+  // return {character_count: charCount, character_limit: charLim};
+}
 
 /**
- * Either fetches the Deepl-Key from const DEEPL_KEY_FILE
+ * Either fetches the Deepl-Key from const deeplKey_FILE
  * or
  * provokes a form window to be shown for the user to input the key
  */
-function getDeeplKey(){
-    storage.get('DeeplKey', (error, data) => {
-        if (error) throw error;
-        if (data) {
-            fetchDeeplCharCount(data.deepl_key);
-        } else {
-            main_process.retrieveDeeplKeyViaWindow();
-        }
-    });
+function getDeeplKey() {
+  storage.get('deeplKey', (error, data) => {
+    if (error) throw error;
+    if (data.deeplKey) {
+      fetchDeeplCharCount(data.deeplKey);
+    } else {
+      mainProcess.retrieveDeeplKeyViaWindow();
+    }
+  });
 }
 
-ipc.on('key_ready', (event, message) => {
-    deepl_key = message[0];
-    if (deepl_key){
-        if (message[1]) {
-            storage.set('DeeplKey', {deepl_key}, (error) => {
-                if (error) throw error;
-            });
-        }
-        fetchDeeplCharCount(deepl_key);
-        const retrieval_window = remote.getGlobal('retrievalWindow');
-        if (retrieval_window) {retrieval_window.webContents.send('key_ack');}
+ipcRenderer.on('key_ready', (event, message) => {
+  const deeplKey = message[0];
+  if (deeplKey) {
+    if (message[1]) {
+      storage.set('deeplKey', { deeplKey }, (error) => {
+        if (error) throw error;
+      });
     }
+    fetchDeeplCharCount(deeplKey);
+    mainProcess.closeRetrievalWindow();
+  }
 });
 
-function fetchDeeplCharCount(key){
-    if (key === null){
-        getDeeplKey();
-        return;
-    }
-    request('https://api.deepl.com/v2/usage?auth_key='+key, function(error, response, body){
-        if(!error && response.statusCode == 200){
-            const values = JSON.parse(body);
-            char_count = values.character_count;
-            char_lim = values.character_limit;
-        }
-        document.getElementById('deepl_character_count').innerHTML = "Characters left on Deepl: "+(char_lim-char_count);
-    });
-    //return {character_count: char_count, character_limit: char_lim};
-}
-
 /**
- * ! The ID of each list element consists of its lastModified-value concatenated with its size - both as strings!
+ * ! The ID of each list element consists of its lastModified-value concatenated with its size
+ * ! - both as strings!
  * @param {event} event The Parameter passed from HTML-ondrop to the script.
  */
 // eslint-disable-next-line no-unused-vars
 
-//TODO check each element if jpeg
-//TODO fetch keywords and caption for each element and write to fields
-function handleDrop(event){
-    function createAndAssignInnerHtml(el_type, inner){
-        let el = document.createElement(el_type);
-        el.innerHTML = inner;
-        return el;
-    }
+// TODO check each element if jpeg
+// TODO fetch keywords and caption for each element and write to fields
+// TODO rewrite function in proper jQuery
+// eslint-disable-next-line no-unused-vars
+function handleDrop(event) {
+  function createAndAssignInnerHtml(elType, inner) {
+    // eslint-disable-next-line no-undef
+    const el = document.createElement(elType);
+    el.innerHTML = inner;
+    return el;
+  }
 
-    event.preventDefault();
-    //hide call, show file-list
-    document.getElementById('call_for_drop').style.display = "none";
-    document.getElementById('file_list').style.display = "table";
-    //add dropped items to list
-    var table_body = document.getElementById('table_body');
-    var files = event.dataTransfer.files;
-    for (var file of files){
-        var new_entry = document.createElement('tr');
-        new_entry.id = (file.lastModified.toString() + file.size.toString());
-        var image_elem = document.createElement('img');
-        image_elem.src = file.path;
-        var children = [
-            createAndAssignInnerHtml('td',file.name),
-            createAndAssignInnerHtml('td', 'N/A'),
-            document.createElement('td').appendChild(image_elem)
-        ];
-        for (var child of children){
-            new_entry.appendChild(child);
-        }
-        table_body.appendChild(new_entry);
-    }
-    /*
-    TODO Active beibehalten wenn clicked, rechtsklick, auswahl, mehrfachauswahl und löschen
-    var all_entries = document.getElementsByTagName('tr');
-    for (entry of all_entries){
-        entry.addEventListener('click', )
-    }
-    */
-    //return false;
+  event.preventDefault();
+  // hide call, show file-list
+  $('#call_for_drop').css('display', 'none');
+  $('#file_list').css('display', 'table');
+  // add dropped items to list
+  const tableBody = $('#tableBody');
+  const { files } = event.dataTransfer;
+  let children = [];
+  for (const file of files) {
+    // TODO: make sure keysAndCaps is not used before it is assigned.
+    // eslint-disable-next-line no-loop-func
+    const reqKeyNCap = Promise.denodeify(metadatahandlerStub.requestKeywordsAndCaption);
+
+    metadatahandlerStub.requestKeywordsAndCaption(file.path, (keysAndCaps) => {
+      console.log(keysAndCaps);
+      // eslint-disable-next-line no-undef
+      const newEntry = document.createElement('tr');
+      newEntry.id = (file.lastModified.toString() + file.size.toString());
+      // eslint-disable-next-line no-undef
+      const imageElem = document.createElement('img');
+      imageElem.src = file.path;
+      children = [
+        createAndAssignInnerHtml('td', file.name),
+        createAndAssignInnerHtml('td', keysAndCaps.caption),
+        createAndAssignInnerHtml('td', keysAndCaps.keywords),
+        createAndAssignInnerHtml('td', metadatahandlerStub.countKeysAndCapChars(keysAndCaps)),
+        // eslint-disable-next-line no-undef
+        document.createElement('td').appendChild(imageElem),
+      ];
+      for (const child of children) {
+        newEntry.appendChild(child);
+      }
+      tableBody.append(newEntry);
+    });
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
-function allowDrop(event){
-    event.preventDefault();
+function allowDrop(event) {
+  event.preventDefault();
 }
-
-function removeFromPictureList(){}
 
 // eslint-disable-next-line no-unused-vars
-function showPreExecutionNotice(){
-    main_process.showPreExecutionNotice();
+function removeFromPictureList() {}
+
+// eslint-disable-next-line no-unused-vars
+function showPreExecutionNotice() {
+  mainProcess.showPreExecutionNotice();
 }
 
+// eslint-disable-next-line no-undef
 window.onload = () => {
-    fetchDeeplCharCount(null);
-}
-
-
+  fetchDeeplCharCount(null);
+};

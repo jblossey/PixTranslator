62a438a9dabf7b78ac965192a8fe4554859539ed
Jannis Blossey <jannis@blossey.eu>
2020-04-05 19:19:24 +0200

    feat: get all by collection of german words

--- backend/databasehandler/src/main/java/com/pixtranslator/backend/databasehandler/TranslateddictionaryRepository.java
+++ backend/databasehandler/src/main/java/com/pixtranslator/backend/databasehandler/TranslateddictionaryRepository.java
@@ -6,12 +6,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "dict", path = "dict")
 public interface TranslateddictionaryRepository extends CrudRepository<Translateddictionary, Serializable> {
 
   Translateddictionary findByGerman(@Param("german") String german);
+
+  List<Translateddictionary> findAllByGermanInIgnoreCase(@Param("german") Collection<String> german);
 
   @Query(value = "SCRIPT TO :file", nativeQuery = true)
   List<String> saveDict(@Param("file") String file);

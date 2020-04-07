b2a2f568c255bdb600dba9228e264dc55b3e8d28
Jannis Blossey <jannis@blossey.eu>
2020-04-07 11:46:41 +0200

    feat: add babeleslint incl private method linting

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,10 +5,38 @@ module.exports = {
         "node": true,
         "jquery": true
     },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "arrowFunctions": true,
+            "binaryLiterals": true,
+            "blockBindings": true,
+            "classes": true,
+            "defaultParams": true,
+            "destructuring": true,
+            "forOf": true,
+            "generators": true,
+            "modules": true,
+            "objectLiteralComputedProperties": true,
+            "objectLiteralDuplicateProperties": true,
+            "objectLiteralShorthandMethods": true,
+            "objectLiteralShorthandProperties": true,
+            "octalLiterals": true,
+            "regexUFlag": true,
+            "regexYFlag": true,
+            "spread": true,
+            "superInFunctions": true,
+            "templateStrings": true,
+            "unicodeCodePointEscapes": true,
+            "globalReturn": true,
+            "jsx": true,
+            "experimentalObjectRestSpread": true,
+            "proposal-private-methods": true
+        }
+      },
     extends: [
       "airbnb-base"
-    ],
-    plugins: [
-      "@babel/plugin-proposal-private-methods"
     ]
 };

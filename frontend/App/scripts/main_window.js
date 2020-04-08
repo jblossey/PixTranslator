4740c26c6b6419dbe4cd2c524eedc2ad272fb8a8
Jannis Blossey <jannis@blossey.eu>
2020-04-08 21:06:25 +0200

    feat: get already translated keywords from database

--- frontend/App/scripts/main_window.js
+++ frontend/App/scripts/main_window.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-continue */
 /* eslint-disable no-cond-assign */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-loop-func */
 /* eslint-disable no-restricted-syntax */
-/* eslint-disable prefer-template */
 const { remote, ipcRenderer } = require('electron');
 const needle = require('needle');
 const storage = require('electron-json-storage');
@@ -12,10 +12,11 @@ const metadatahandlerStub = require('./scripts/metadatahandlerStub');
 // eslint-disable-next-line import/no-unresolved
 const translator = require('./scripts/translationRoutine');
 
-
 const mainProcess = remote.require('./main.js');
 
 // '27c44ce2-ddbb-47ed-e8c6-1809382b6000';// TODO delete
+
+let picCollectionArray = [];
 
 let charCount;
 let charLim;
@@ -28,13 +29,13 @@ function fetchDeeplCharCount(key) {
     getDeeplKey();
     return;
   }
-  needle.get('https://api.deepl.com/v2/usage?auth_key=' + key, (error, response) => {
+  needle.get(encodeURI(`https://api.deepl.com/v2/usage?auth_key=${key}`), (error, response) => {
     if (!error && response.statusCode === 200) {
       charCount = response.body.character_count;
       charLim = response.body.character_limit;
     }
     // eslint-disable-next-line no-undef
-    $('#deepl_character_count').text('Characters left on Deepl: ' + (charLim - charCount));
+    $('#deepl_character_count').text(`Characters left on Deepl: ${charLim - charCount}`);
   });
   // return {character_count: charCount, character_limit: charLim};
 }
@@ -97,19 +98,27 @@ function handleDrop(event) {
   let children = [];
   let totalCharCount = 0;
   for (const file of files) {
+    // eslint-disable-next-line no-undef
+    if (document.getElementById(file.path.replace(/\s/g, ''))) continue; // skip if element already exists
     metadatahandlerStub.requestKeywordsAndCaption(file.path).then((keysAndCaps) => {
+      const currentPicCollection = new translator.PicCollection(
+        file.path,
+        keysAndCaps.keywords,
+        keysAndCaps.caption,
+      );
+      picCollectionArray.push(currentPicCollection);
+      const currentCharCount = metadatahandlerStub.countKeysAndCapChars(keysAndCaps);
       // eslint-disable-next-line no-undef
       const newEntry = document.createElement('tr');
-      newEntry.id = (file.lastModified.toString() + file.size.toString());
+      newEntry.id = file.path.replace(/\s/g, '');
       // eslint-disable-next-line no-undef
       const imageElem = document.createElement('img');
       imageElem.src = file.path;
-      const currentCharCount = metadatahandlerStub.countKeysAndCapChars(keysAndCaps);
       totalCharCount += currentCharCount;
       children = [
         createAndAssignInnerHtml('td', file.name),
-        createAndAssignInnerHtml('td', keysAndCaps.caption),
-        createAndAssignInnerHtml('td', keysAndCaps.keywords),
+        createAndAssignInnerHtml('td', currentPicCollection.caption),
+        createAndAssignInnerHtml('td', currentPicCollection.keywords),
         createAndAssignInnerHtml('td', currentCharCount),
         // eslint-disable-next-line no-undef
         document.createElement('td').appendChild(imageElem),
@@ -118,7 +127,7 @@ function handleDrop(event) {
         newEntry.appendChild(child);
       }
       tableBody.append(newEntry);
-      $('#picture_character_count').text('Characters in Pictures: ' + totalCharCount);
+      $('#picture_character_count').text(`Characters in Pictures: ${totalCharCount}`);
     });
   }
 }
@@ -133,6 +142,7 @@ function removeAllItemsFromTable() {
   $('#table_body').empty();
   $('#file_list').css('display', 'none');
   $('#call_for_drop').css('display', 'initial');
+  picCollectionArray = [];
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -141,33 +151,22 @@ function showPreExecutionNotice() {
 }
 
 // eslint-disable-next-line no-unused-vars
-ipcRenderer.on('startTranslation', (event) => {
+ipcRenderer.on('startTranslation', async (event) => {
   mainProcess.showProgressWindow();
   const progressWindow = remote.getGlobal('progressWindow');
   // eslint-disable-next-line no-undef
   const totalPicNumber = document.getElementById('table_body').rows.length;
   if (progressWindow) progressWindow.webContents.send('initProgressbar', [totalPicNumber]);
-  const PicCollectionArray = translator.collectKeysAndCaps();
+  picCollectionArray = await translator.getDbTranslationsForMany(picCollectionArray);
   // TODO
 });
 
-const checkMetadatahandlerHealth = () => {
-  needle.get('http://localhost:4711/actuator/health', (err, response) => {
+const checkServiceHealth = (service, port) => {
+  needle.get(`http://localhost:${port}/actuator/health`, (err, response) => {
     if (err || response.statusCode !== 200) {
-      $('#metadatahandler_status').css('color', 'red');
+      $(`#${service}_status`).css('color', 'red');
     } else {
-      $('#metadatahandler_status').css('color', 'lime');
-    }
-  });
-};
-
-const dumpDatabase = () => {
-  needle.get('http://localhost:4712/dumpDictionary', (err, response) => {
-    if (err || response.statusCode !== 200) {
-      $('#database_status').css('color', 'red');
-    } else {
-      $('#database_status').css('color', 'lime');
-      $('#lastSave').text('Last Save: ' + new Date().toLocaleTimeString('en-GB'));
+      $(`#${service}_status`).css('color', 'lime');
     }
   });
 };
@@ -176,13 +175,13 @@ const dumpDatabase = () => {
  * Bundled place for all regularly occuring checks, routines, etc.
  */
 const intervalFunctions = () => {
-  setInterval(() => { checkMetadatahandlerHealth(); }, 10000);
-  setInterval(() => { dumpDatabase(); }, 30000);
+  setInterval(() => { checkServiceHealth('metadatahandler', 4711); }, 10000);
+  setInterval(() => { checkServiceHealth('databasehandler', 4712); }, 10000);
 };
 
 const initIntervals = () => {
-  checkMetadatahandlerHealth();
-  dumpDatabase();
+  checkServiceHealth('metadatahandler', 4711);
+  checkServiceHealth('databasehandler', 4712);
   intervalFunctions();
 };
 
--- frontend/App/scripts/translationRoutine.js
+++ frontend/App/scripts/translationRoutine.js
@@ -1,33 +1,103 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-cond-assign */
+/* eslint-disable no-plusplus */
 const { remote } = require('electron');
+const Promise = require('promise');
+const needle = require('needle');
 
 class PicCollection {
-  #picPath;
-  #keywords;
-  #caption;
-  #translatedKeywords;
-  #translatedCaption;
-  #toSend;
+  picPath;
+
+  keywords;
+
+  caption;
+
+  translatedKeywords;
+
+  translatedCaption;
+
+  toSend
+
   constructor(picPath, keywords, caption) {
-    this.#picPath = picPath;
-    this.#keywords = keywords;
-    this.#caption = caption;
-    this.#translatedKeywords = [];
-    this.#translatedCaption = "";
-    this.#toSend = [caption];
+    this.picPath = picPath;
+    this.keywords = keywords;
+    this.caption = caption;
+    this.translatedKeywords = [];
+    this.translatedCaption = '';
+    this.toSend = [caption];
   }
 
-  get picPath() { return this.#picPath }
-  get keywords() { return this.#keywords }
-  get caption() { return this.#caption }
-  get translatedKeywords() { return this.#translatedKeywords }
-  get translatedCaption() { return this.#translatedCaption }
-  get toSend() { /*TODO*/ }
+  get picPath() { return this.picPath; }
 
-  set translatedKeywords(translated) { this.#translatedKeywords.push(translated).flat(Infinity) }
-  set translatedCaption(translated) { this.#translatedCaption = translated }
+  get keywords() { return this.keywords; }
 
+  get caption() { return this.caption; }
+
+  get translatedKeywords() { return this.translatedKeywords; }
+
+  get translatedCaption() { return this.translatedCaption; }
+
+  get toSend() { return this.toSend; }
+
+  set translatedKeywords(translated) { this.translatedKeywords.push(translated).flat(Infinity); }
+
+  set translatedCaption(translated) { this.translatedCaption = translated; }
+
+  set toSend(untranslated) { this.toSend.push(untranslated).flat(Infinity); }
 }
 
-exports.collectKeysAndCaps = () => {
-  // TODO
+const extractTranslationFromDatabaseCall = (dbResponse) => {
+  const translations = [];
+  for (let i = 0, translated; translated = dbResponse._embedded.dict[i]; i++) {
+    translations.push(translated.english);
+  }
+  return translations;
+};
+
+const setDifference = (minuend, subtrahend) => minuend.filter((x) => !subtrahend.includes(x));
+
+const extractUntranslatedKeywords = (originalCollection, rawDbResponse) => {
+  const untranslatedArray = [];
+  for (let i = 0, untranslated; untranslated = rawDbResponse._embedded.dict[i]; i++) {
+    untranslatedArray.push(untranslated.german);
+  }
+  return setDifference(originalCollection.keywords, untranslatedArray);
+};
+
+const getDbTranslationsForOne = (picCollection) => new Promise((fulfill, reject) => {
+  let request = 'http://localhost:4712/dict/search/findAllByGermanInIgnoreCase?';
+  const { keywords } = picCollection;
+  for (let i = 0, keyword; keyword = keywords[i]; i++) {
+    request += `german=${encodeURIComponent(keyword)}&`;
+  }
+  // needle.request()
+  needle('get', request).then((response) => {
+    try {
+      if (response.statusCode === 200) {
+        const jsonBody = JSON.parse(response.body);
+        picCollection.translatedKeywords = extractTranslationFromDatabaseCall(jsonBody);
+        picCollection.toSend = extractUntranslatedKeywords(picCollection, jsonBody);
+        fulfill(picCollection);
+      }
+      // TODO proper handling of single outages during db call
+    } catch (error) {
+      reject(error);
+    }
+  });
+});
+
+const getDbTranslationsForMany = async (picCollectionArray) => {
+  const progressWindow = remote.getGlobal('progressWindow');
+  await Promise.all(picCollectionArray.map(async (currentPicCollection) => {
+    await getDbTranslationsForOne(currentPicCollection);
+    if (progressWindow) progressWindow.webContents.send('progressStep');
+  }));
+  return picCollectionArray;
+};
+
+module.exports = {
+  PicCollection,
+  getDbTranslationsForMany,
+  getDbTranslationsForOne,
 };

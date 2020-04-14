5cf5c819ca060fcff1bf38f9660728e87e340ae1
Jannis Blossey <jannis@blossey.eu>
2020-04-15 00:58:37 +0200

    feat: setup CI

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd backend
+
+mvn clean install package
+
+mv ./metadatahandler/target/*{.jar,.exe} ../frontend
+mv ./databasehandler/target/*{.jar,.exe} ../frontend
+
+cd ../frontend
+
+docker run --rm \
+    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
+    -v ${PWD}:/project \
+    -v ~/.cache/electron:/root/.cache/electron \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+    electronuserland/builder:wine \
+    yarn && yarn dist --linux --win

d65af6250b001ee420f00e495ccee4fa0240eccc
Jannis Blossey <jannis@blossey.eu>
2020-04-15 14:24:07 +0200

    add echos for debugging

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 cd backend
+echo "enter backend"
 
+echo "start maven"
 mvn clean install package
 
+echo "move files"
 mv ./metadatahandler/target/*{.jar,.exe} ../frontend
 mv ./databasehandler/target/*{.jar,.exe} ../frontend
 
+echo "entering frontend"
 cd ../frontend
 
+echo "starting frontend build"
 docker run --rm \
     --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
     -v ${PWD}:/project \

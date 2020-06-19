f696d7c8bac0a2c2e695ad8321201ad55aa5dd3b
Jannis Blossey <jannis@blossey.eu>
2020-06-19 14:02:05 +0200

    chore: cleanup adds

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ mvn clean install package
 echo "move files"
 mv ./metadatahandler/target/*{.jar,.exe} ../frontend
 mv ./databasehandler/target/*{.jar,.exe} ../frontend
+mvn clean
 
 echo "entering frontend"
 cd ../frontend
@@ -21,3 +22,6 @@ docker run --rm \
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
     electronuserland/builder:wine \
     /bin/bash -c "yarn && yarn dist && chown -R 1000:1000 ."
+
+rm databasehandler*
+rm metadatahandler*
